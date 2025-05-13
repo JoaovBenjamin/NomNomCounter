bc4e87ef1949071801ee0f292c22e00b76ebfeff
Joaovvb <rm98938@fiap.com.br>
2025-05-13 16:16:11 -0300

    first commit

---
+++ src/main/java/com/api/NomNomCounter/NomNomCounterApplication.java
@@ -0,0 +1,13 @@
+package com.api.NomNomCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomNomCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomNomCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/NomNomCounter/NomNomCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.NomNomCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
